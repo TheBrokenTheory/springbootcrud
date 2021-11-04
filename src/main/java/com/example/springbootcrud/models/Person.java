9fa81ef4fcb0a05e5fde9a902d77214a5c1efa90
Jonathan <Jonathananders6@gmail.com>
2021-11-04 13:21:28 -0400

    First Test

--- src/main/java/com/example/springbootcrud/models/Person.java
+++ src/main/java/com/example/springbootcrud/models/Person.java
@@ -10,7 +10,7 @@ public class Person {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private Integer id;
     private String name;
     private String address;
     private String phone;
